8aa2e176bc773b843585a251f51f8b3b44f0c269
pavlen5kiy <wolkpavel51@gmail.com>
2024-04-15 18:36:06 +0300

    commit

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
+#!/usr/bin/python
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+
+import telebot
+
+API_TOKEN = '7161683104:AAHqONitwtt3N4x2Wx-2MFMxC55f-d0ShVU'
+
+bot = telebot.TeleBot(API_TOKEN)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am MusicTheoryBot.
+I am here to help you with music theory. Maybe you want me to build a scale or give you some basic knowledges. Whatever you want!\
+""")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.send_audio(message, "Sorry, I don't get it")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == "__main__":
+    bot.polling()
