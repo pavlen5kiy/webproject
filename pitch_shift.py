7d18363da85b0b721369ef0cb71263ce60faf458
Bebront <151544955+Bebront@users.noreply.github.com>
2024-04-21 18:57:29 +0300

    Add files via upload
    Коммит с моей частью работы

---
+++ pitch_shift.py
@@ -0,0 +1,102 @@
+import librosa
+import soundfile as sf
+from variables import NOTES
+
+intervals_files = ['min_sec', 'maj_sec',
+                   'min_third', 'maj_third',
+                   'fourth', 'tritone', 'fifth',
+                   'min_sixth', 'maj_sixth',
+                   'min_seventh', 'maj_seventh',
+                   'octave']
+
+note_answers = {
+    0: 'C',
+    1: 'C#\Db',
+    2: 'D',
+    3: 'D#\Eb',
+    4: 'E',
+    5: 'F',
+    6: 'F#\Gb',
+    7: 'G',
+    8: 'G#\Ab',
+    9: 'A',
+    10: 'A#\Bb',
+    11: 'B\Cb'
+}
+
+FILES_TO_INTERVALS = {
+    'min_sec': 'Minor 2nd',
+    'maj_sec': 'Major 2nd',
+    'min_third': 'Minor 3rd',
+    'maj_third': 'Major 3rd',
+    'fourth': '4th',
+    'tritone': 'Tritone',
+    'fifth': '5th',
+    'min_sixth': 'Minor 6th',
+    'maj_sixth': 'Major 6th',
+    'min_seventh': 'Minor 7th',
+    'maj_seventh': 'Major 7th',
+    'octave': 'Octave'
+}
+
+INTERVALS_TO_FILES = {
+    'Minor 2nd': 'min_sec',
+    'Major 2nd': 'maj_sec',
+    'Minor 3rd': 'min_third',
+    'Major 3rd': 'maj_third',
+    '4th': 'fourth',
+    'Tritone': 'tritone',
+    '5th': 'fifth',
+    'Minor 6th': 'min_sixth',
+    'Major 6th': 'maj_sixth',
+    'Minor 7th': 'min_seventh',
+    'Major 7th': 'maj_seventh',
+    'Octave': 'octave'
+}
+
+
+def note_shift(semitones):
+    audio_file = "data/c_note.mp3"
+    y, sr = librosa.load(audio_file)
+
+    y_shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=semitones)
+
+    output_file = "note.mp3"
+    sf.write(output_file, y_shifted, samplerate=sr)
+
+
+def interval_shift(interval, semitones):
+    audio_file = f'data/{interval}.mp3'
+    y, sr = librosa.load(audio_file)
+
+    y_shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=semitones)
+
+    output_file = "interval.mp3"
+    sf.write(output_file, y_shifted, samplerate=sr)
+
+
+def get_shifted_scale(scale, root):
+    audio_file = f'data/{scale}.mp3'
+    y, sr = librosa.load(audio_file)
+
+    semitones = NOTES.index(root)
+
+    y_shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=semitones)
+
+    output_file = "scale.mp3"
+    sf.write(output_file, y_shifted, samplerate=sr)
+
+
+def get_shifted_chord(root, base, add=''):
+    audio_file = f'data/{base}.mp3'
+    if add:
+        audio_file = f'data/{base}_{add}.mp3'
+
+    y, sr = librosa.load(audio_file)
+
+    semitones = NOTES.index(root)
+
+    y_shifted = librosa.effects.pitch_shift(y, sr=sr, n_steps=semitones)
+
+    output_file = "chord.mp3"
+    sf.write(output_file, y_shifted, samplerate=sr)
